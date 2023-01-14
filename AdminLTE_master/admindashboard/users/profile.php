2b8cffac769d19af4f59e0c6e9eee7469847c18a
AmroAl-Wageei <shandaqamro@gmail.com>
2023-01-14 21:16:42 +0300

    new_aboutAdmin

--- AdminLTE_master/admindashboard/users/profile.php
+++ AdminLTE_master/admindashboard/users/profile.php
@@ -18,86 +18,14 @@ session_start();
     }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Profile</title>
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
-</head>
-<body class="hold-transition sidebar-mini" style="background-color: #eee;">
+<?php include "../../components/header.php" ?>
 
-    <nav class="navbar navbar-expand navbar-white navbar-light">
-        <!-- Left navbar links -->
-        <ul class="navbar-nav">
-          <!-- <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-          </li> -->
-          <!-- <li class="nav-item d-none d-sm-inline-block">
-            <a href="../../index3.html" class="nav-link">Home</a>
-          </li> -->
-          <!-- <li class="nav-item d-none d-sm-inline-block">
-            <a href="#" class="nav-link">Contact</a>
-          </li> -->
-          <li class="nav-item d-none d-sm-inline-block">
-            <a href="http://localhost/PHP_PROJECT/" class="nav-link">Home</a>
-          </li>
-            <li class="nav-item">
-            <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-              <i class="fas fa-expand-arrows-alt"></i>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-              <i class="fas fa-th-large"></i>
-            </a>
-          </li>
-        </ul>
-    
-        <!-- Right navbar links -->
-        <ul class="navbar-nav ml-auto">
-          <!-- Navbar Search -->
-          <!-- <li class="nav-item">
-            <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-              <i class="fas fa-search"></i>
-            </a>
-            <div class="navbar-search-block">
-              <form class="form-inline">
-                <div class="input-group input-group-sm">
-                  <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-                  <div class="input-group-append">
-                    <button class="btn btn-navbar" type="submit">
-                      <i class="fas fa-search"></i>
-                    </button>
-                    <button class="btn btn-navbar" type="button" data-widget="navbar-search">
-                      <i class="fas fa-times"></i>
-                    </button>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </li> -->
-          <!-- <li class="nav-item d-none d-sm-inline-block">
-            <a href="../../index3.html" class="nav-link">Home</a>
-          </li>
-            <li class="nav-item">
-            <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-              <i class="fas fa-expand-arrows-alt"></i>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-              <i class="fas fa-th-large"></i>
-            </a>
-          </li> -->
-        </ul>
-      </nav>
+
+
+
+             
+
+
 
 <section class="content">
     <div class="container-fluid">
@@ -118,23 +46,14 @@ session_start();
 
               <p class="text-muted text-center"><?php echo $user['is_admin'] ? 'super user' : 'user' ?></p>
 
-              <ul class="list-group list-group-unbordered mb-3">
-                <li class="list-group-item">
-                  <b>Orders</b> <a class="float-right">14</a>
-                </li>
-                <li class="list-group-item">
-                  <b>Reviews</b> <a class="float-right">5</a>
-                </li>
-              </ul>
 
-              <a href="http://localhost/PHP_PROJECT/AdminLTE_master/admindashboard/users/update_user.php?id=<?php echo $_SESSION['user_id']; ?>" class="btn btn-primary btn-block"><b>Edit</b></a>
-            </div>
-            <!-- /.card-body -->
-          </div>
-          <!-- /.card -->
 
+<div class="container">
+
+    <!-- <nav class="navbar navbar-expand navbar-white navbar-light"> -->
+        <!-- Left navbar links -->
           <!-- About Me Box -->
-          <div class="card card-primary">
+          <div class="card card-primary d-flex justify-content-center" style="margin: 15%;">
             <div class="card-header">
               <h3 class="card-title">About Me</h3>
             </div>
@@ -164,11 +83,25 @@ session_start();
       </div>
       <!-- /.row -->
     </div><!-- /.container-fluid -->
+
   </section>
 
-  <footer class="main-footer" style="margin-left: 0;">
+  </div>
+
+
+  <!-- FOOTER -->
+
+
+
+
+
+
+
+
+  <!-- /.content-wrapper -->
+  <footer class="main-footer" style="text-align: center;">
     <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.2.0
+      
     </div>
     <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
   </footer>
@@ -189,5 +122,9 @@ session_start();
 <script src="../../dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="../../dist/js/demo.js"></script>
+
+
+
+
 </body>
 </html>
